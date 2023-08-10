4586bf5e2e11469b583ec2586d99a3ffc1bac009
jemiyosua <jemiyosua@gmail.com>
2023-08-10 23:53:35 +0700

    update minor file

--- tsukamoto/index.php
+++ tsukamoto/index.php
@@ -36,6 +36,24 @@
 
 		<div class="container">
 
+		<div style="padding-top:30px"></div>
+
+<?php
+
+if (isset($_POST["submit"]) || isset($_POST["submit2"])) {
+
+?>
+
+			<a href="index.php" type="button" class="btn btn-danger" name="submit">Reset</a>	
+
+			<div style="padding-bottom:30px"></div>
+
+<?php
+
+} else {
+
+?>
+
 			<form method="POST" action="index.php">
 				<div class="mb-3">
 					<label for="exampleFormControlInput1" 	class="form-label">Jumlah Pencucian</label>
@@ -45,7 +63,11 @@
 				<button type="submit" class="btn btn-success" name="submit">Submit</button>
 			</form>
 
+			
+
 <?php
+
+}
 
 if (isset($_POST["submit"])) {
 	$jumlah_pencucian = $_POST["jumlah_pencucian"];
@@ -55,255 +77,47 @@ if ($jumlah_pencucian < 2) {
 	exit;
 }
 
-$takaran = array();
-function FungsiInferensiSedikit($variabel_kain, $variabel_kekotoran, $variabel_warna, $variabel_bobot) {
-	global $takaran;
-	if ($variabel_kain != 0) {
-		if ($variabel_kekotoran != 0) {
-			if ($variabel_warna != 0) {
-				if ($variabel_bobot != 0) {
-					$hasil_output = min($variabel_kain, $variabel_kekotoran, $variabel_warna, $variabel_bobot);
-					array_push($takaran, [$hasil_output, 40]);
-				}
-			}
-		}
-	}
-}
-
-function FungsiInferensiBanyak($variabel_kain, $variabel_kekotoran, $variabel_warna, $variabel_bobot) {
-	global $takaran;
-	if ($variabel_kain != 0) {
-		if ($variabel_kekotoran != 0) {
-			if ($variabel_warna != 0) {
-				if ($variabel_bobot != 0) {
-					$hasil_output = min($variabel_kain, $variabel_kekotoran, $variabel_warna, $variabel_bobot);
-					array_push($takaran, [$hasil_output, 80]);
-				}
-			}
-		}
-	}
-}
-
-for ($i=1; $i <= $jumlah_pencucian; $i++) { 
-
-	if ($a_kain[$i] == "") {
-
-	?>
-
-		<form method="POST" action="index.php">
-			<div class="mb-3">
-				<label for="exampleFormControlInput1" 	class="form-label">Ketebalan Kain Pencucian <?= $i ?></label>
-				<input type="number" class="form-control" name="<?= $i ?>">
-			</div>
-
-			<!-- <button type="submit" class="btn btn-success" name="submit">Submit</button> -->
-		</form>
-
-	<?php
-
-	} else if ($b_kekotoran[$i] == "") {
-
-	?>
-
-		<form method="POST" action="index.php">
-			<div class="mb-3">
-				<label for="exampleFormControlInput1" 	class="form-label">Tingkat Kekotoran</label>
-				<input type="number" class="form-control" placeholder="Input Jumlah Pencucian" name="<?= $i ?>">
-			</div>
-
-			<!-- <button type="submit" class="btn btn-success" name="submit">Submit</button> -->
-		</form>
-
-	<?php
-
-	} else if ($c_warna[$i] == "") {
-
-	?>
-	
-		<form method="POST" action="index.php">
-			<div class="mb-3">
-				<label for="exampleFormControlInput1" 	class="form-label">Warna Kain</label>
-				<input type="number" class="form-control" placeholder="Input Jumlah Pencucian" name="jumlah_pencucian">
-			</div>
-
-			<button type="submit" class="btn btn-success" name="submit">Submit</button>
-		</form>
-
-	<?php
-
-	}
-
-	$a_kain = 5;
-	$b_kekotoran = 5;
-	$c_warna = 5;
-	$d_bobot = 5;
-
-	$kain = $a_kain;
-	$kekotoran = $b_kekotoran;
-	$warna = $c_warna;
-	$bobot = $d_bobot;
-
-	// ------ start of ketebalan -------
-	if ($kain <= 3) {
-		$value_tipis = 1;
-		$value_sedang = 0;
-		$value_tebal = 0;
-	}
-	if ($kian > 3 && $kain < 5) {
-		$value_tipis = (5-$kain)/(5-3);
-		$value_sedang = ($kain - 3)/(5-3);
-		$value_tebal = 0;
-	}
-	if ($kain == 5) {
-		$value_tipis = 0;
-		$value_sedang = 1;
-		$value_tebal = 0;
-	}
-	if ($kain > 5 && $kain < 0) {
-		$value_tipis = 0;
-		$value_sedang = (8 - $kain)/(8-5);
-		$value_tebal = ($kain - 5)/(8-5);
-	}
-	if ($kain >= 8) {
-		$value_tipis = 0;
-		$value_sedang = 0;
-		$value_tebal = 1;
-	}
-	// ------ end of ketebalan -------
-	
-	// ------ start of kekotoran -------
-	if ($kekotoran <= 3) {
-		$value_rendah = 1;
-		$value_tinggi = 0;
-	}
-	if ($kekotoran > 3 && $kekotoran < 8) {
-		$value_rendah = (8 - $kekotoran)/(8 - 3);
-		$value_tinggi = ($kekotoran - 3)/(8 - 3);
-	}
-	if ($kekotoran >= 8) {
-		$value_rendah = 0;
-		$value_tinggi = 1;
-	}
-	// ------ end of kekotoran -------
-
-	// ------ start of warna -------
-	if ($warna <= 3) {
-		$value_gelap = 1;
-		$value_sedang = 0;
-		$value_terang = 0;
-	}
-	if ($warna > 3 && $warna < 5) {
-		$value_gelap = (5 - $warna)/(5 - 3);
-		$value_sedang = ($warna - 3)/(5 - 3);
-		$value_terang = 0;
-	}  
-	if ($warna == 5) {
-		$value_gelap = 0;
-		$value_sedang = 1;
-		$value_terang = 0;
-	}   
-	if ($warna > 5 && $warna < 8) {
-		$value_gelap = 0;
-		$value_sedang = (8 - $warna)/(8 - 5);
-		$value_terang = ($warna - 5)/(8 - 5);
-	}  
-	if ($warna >= 8) {
-		$value_gelap = 0;
-		$value_sedang = 0;
-		$value_terang = 1;
-	}
-	// ------ end of warna -------
-
-	// ------ start of berat -------
-	if ($bobot <= 3) {
-		$value_ringan = 1;
-		$value_berat = 0;
-	}
-	if ($bobot > 3 && $bobot < 8) {
-		$value_ringan = (8 - $bobot)/(8 - 3);
-		$value_berat = ($bobot - 3)/(8 - 3);
-	}   
-	if ($bobot >= 8) {
-		$value_ringan = 0;
-		$value_berat = 1;
-	}
-	// ------ end of berat -------
-
-	// RulesSedikit
-	// 1-6
-	FungsiInferensiSedikit($value_tipis, $value_rendah, $value_terang, $value_ringan);
-	FungsiInferensiSedikit($value_tipis, $value_rendah, $value_terang, $value_berat);
-	FungsiInferensiSedikit($value_tipis, $value_rendah, $value_sedang, $value_ringan);
-	FungsiInferensiSedikit($value_tipis, $value_rendah, $value_sedang, $value_berat);
-	FungsiInferensiSedikit($value_tipis, $value_rendah, $value_gelap, $value_ringan);
-	FungsiInferensiSedikit($value_tipis, $value_rendah, $value_gelap, $value_berat);
-
-	// 13-18
-	FungsiInferensiSedikit($value_sedang, $value_rendah, $value_terang, $value_ringan);
-	FungsiInferensiSedikit($value_sedang, $value_rendah, $value_terang, $value_berat);
-	FungsiInferensiSedikit($value_sedang, $value_rendah, $value_sedang, $value_ringan);
-	FungsiInferensiSedikit($value_sedang, $value_rendah, $value_sedang, $value_berat);
-	FungsiInferensiSedikit($value_sedang, $value_rendah, $value_gelap, $value_ringan);
-	FungsiInferensiSedikit($value_sedang, $value_rendah, $value_gelap, $value_berat);
-	// 25-30
-	FungsiInferensiSedikit($value_tebal, $value_rendah, $value_terang, $value_ringan);
-	FungsiInferensiSedikit($value_tebal, $value_rendah, $value_terang, $value_berat);
-	FungsiInferensiSedikit($value_tebal, $value_rendah, $value_sedang, $value_ringan);
-	FungsiInferensiSedikit($value_tebal, $value_rendah, $value_sedang, $value_berat);
-	FungsiInferensiSedikit($value_tebal, $value_rendah, $value_gelap, $value_ringan);
-	FungsiInferensiSedikit($value_tebal, $value_rendah, $value_gelap, $value_berat);
-
-	// RulesBanyak
-	// 7-12
-	FungsiInferensiBanyak($value_tipis, $value_tinggi, $value_terang, $value_ringan);
-	FungsiInferensiBanyak($value_tipis, $value_tinggi, $value_terang, $value_berat);
-	FungsiInferensiBanyak($value_tipis, $value_tinggi, $value_sedang, $value_ringan);
-	FungsiInferensiBanyak($value_tipis, $value_tinggi, $value_sedang, $value_berat);
-	FungsiInferensiBanyak($value_tipis, $value_tinggi, $value_gelap, $value_ringan);
-	FungsiInferensiBanyak($value_tipis, $value_tinggi, $value_gelap, $value_berat);
-	// 19-24
-	FungsiInferensiBanyak($value_sedang, $value_tinggi, $value_terang, $value_ringan);
-	FungsiInferensiBanyak($value_sedang, $value_tinggi, $value_terang, $value_berat);
-	FungsiInferensiBanyak($value_sedang, $value_tinggi, $value_sedang, $value_ringan);
-	FungsiInferensiBanyak($value_sedang, $value_tinggi, $value_sedang, $value_berat);
-	FungsiInferensiBanyak($value_sedang, $value_tinggi, $value_gelap, $value_ringan);
-	FungsiInferensiBanyak($value_sedang, $value_tinggi, $value_gelap, $value_berat);
-	// 31-36
-	FungsiInferensiBanyak($value_tebal, $value_tinggi, $value_terang, $value_ringan);
-	FungsiInferensiBanyak($value_tebal, $value_tinggi, $value_terang, $value_berat);
-	FungsiInferensiBanyak($value_tebal, $value_tinggi, $value_sedang, $value_ringan);
-	FungsiInferensiBanyak($value_tebal, $value_tinggi, $value_sedang, $value_berat);
-	FungsiInferensiBanyak($value_tebal, $value_tinggi, $value_gelap, $value_ringan);
-	FungsiInferensiBanyak($value_tebal, $value_tinggi, $value_gelap, $value_berat);
-
-	for ($j=0; $j < count($takaran); $j++) { 
-		$perkalian = $takaran[$j][0]*$takaran[$j][1];
-		$pembagian = $takaran[$j][0];
-		$perkalian_new += $perkalian;
-		$pembagian_new += $pembagian;
-		$z = $perkalian_new / $pembagian_new;
-	}
-	?>
-
-	<table class="table table-hover" style="text-align:center">
-		<thead class="thead-dark">
-			<tr>
-				<th scope="col">Takaran Deterjen Ke - <?= $i ?></th>
-			</tr>
-		</thead>
-		<tbody>
-			<tr>
-				<td><?= $z ?></td>
-			</tr>
-		</tbody>
-	</table>
-
-	<?php
-}
-			
 ?>
 
-		
+			<form method="POST" action="index2.php">
+
+<?php
+
+for ($i=1; $i <= $jumlah_pencucian; $i++) {
+
+?>
+    
+				<div class="mb-3" style="width:50%">
+					<label class="form-label">Ketebalan Kain Pencucian <?= $i ?></label>
+					<input type="number" class="form-control" name="ketebalan_kain_<?=$i?>">
+				</div>
+
+				<div class="mb-3" style="width:50%">
+					<label class="form-label">Tingkat Kekotoran <?= $i ?></label>
+					<input type="number" class="form-control" name="tingkat_kekotoran_<?=$i?>">
+				</div>
+
+				<div class="mb-3" style="width:50%">
+					<label class="form-label">Warna Kain <?= $i ?></label>
+					<input type="number" class="form-control" name="warna_kain_<?=$i?>">
+				</div>
+
+				<div class="mb-3" style="width:50%">
+					<label class="form-label">Bobot Cucian <?= $i ?></label>
+					<input type="number" class="form-control" name="bobot_cucian_<?=$i?>">
+				</div>
+
+<?php
+
+}
+
+?>
+
+				<input type="hidden" class="form-control" name="jumlah_pencucian" value="<?=$jumlah_pencucian?>">
+
+				<button type="submit" class="btn btn-success" name="submit">Submit</button>
+
+			</form>
 
 		</div>
 
